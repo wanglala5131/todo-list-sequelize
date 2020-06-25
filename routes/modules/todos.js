a0e1c45a60cb2fef590bf17aed9bd4f1aa691eec
Wanglala <sue4100035045@gmail.com>
2020-06-25 16:58:31 +0800

    feat: add create function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -5,11 +5,24 @@ const User = db.User
 const Todo = db.Todo
 
 
+router.get('/new', (req, res) => {
+  res.render('new')
+})
+router.post('/new', (req, res) => {
+  const { name } = req.body
+  const UserId = req.user.id
+  Todo.create({ name, UserId })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
+})
+
 router.get('/:id', (req, res) => {
   const id = req.params.id
   return Todo.findByPk(id)
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
     .catch(error => console.log(error))
 })
+
+
 
 module.exports = router
