98f3db9469c8458d10ac1b480101358b7ceff423
Wanglala <sue4100035045@gmail.com>
2020-06-25 17:24:41 +0800

    feat: add edit function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -15,6 +15,25 @@ router.post('/new', (req, res) => {
     .then(() => res.redirect('/'))
     .catch(err => console.log(err))
 })
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('edit', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+router.put('/:id', (req, res) => {
+  const UserId = req.user.id
+  const { name, isDone } = req.body
+  const { id } = req.params
+  Todo.findOne({ where: { id, UserId } })
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      return todo.save()
+    })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
+})
 
 router.get('/:id', (req, res) => {
   const id = req.params.id
